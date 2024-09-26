ffdacc21dcd3ec6f9c2892f0eea495a97eaa842d
Julian Kennon <julian.kennon.d@gmail.com>
2024-09-25 19:09:54 -0700

    add representatives table

---
+++ db/migrate/20240926015647_create_representatives.rb
@@ -0,0 +1,14 @@
+class CreateRepresentatives < ActiveRecord::Migration[7.1]
+  def change
+    create_table :representatives do |t|
+      t.string :name
+      t.string :chamber
+      t.string :bioguide_id, index: { unique: true }
+      t.boolean :current_member
+      t.timestamps
+    end
+
+    add_reference :representatives, :state, null: false, foreign_key: true
+  end
+end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_26_012229) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_26_015647) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "representatives", force: :cascade do |t|
+    t.string "name"
+    t.string "chamber"
+    t.string "bioguide_id"
+    t.boolean "current_member"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "state_id", null: false
+    t.index ["bioguide_id"], name: "index_representatives_on_bioguide_id", unique: true
+    t.index ["state_id"], name: "index_representatives_on_state_id"
+  end
 
   create_table "states", force: :cascade do |t|
     t.string "name"
@@ -22,4 +34,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_26_012229) do
     t.index ["abbreviation"], name: "index_states_on_abbreviation", unique: true
   end
 
+  add_foreign_key "representatives", "states"
 end
